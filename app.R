91fdb5d76aa5eebc3d727426fa94d483d8adaa5f
Joako55 <Garbarian5@gmail.com>
2017-09-25 21:46:08 -0300

    Menu Items added and graphs

--- app.R
+++ app.R
@@ -1,5 +1,7 @@
 #################################################Data Manipulation#######################################################
 #Load required librarys.
+library(shiny)
+library(shinydashboard)
 library(purrr)
 library(dplyr)
 library(leaflet)
@@ -7,10 +9,9 @@ library(magrittr)
 library(rgdal)
 library(readxl)
 library(lubridate)
+library(DT)
 library(readr)
-library(shiny)
-library(shinydashboard)
-
+library(ggplot2)
 
 
 #Makes the market icon for the map.
@@ -30,21 +31,31 @@ Puntos<-mutate(Supers,Visible = 1)
 
 #Loads the coordenates file from clientes, Select fullcoord.xlsx .
 fullcoord <- read_excel("data/fullcoord.xlsx", 
-                        sheet = "fullcoord", col_types = c("blank", 
+                        sheet = "fullcoord", col_types = c("skip", 
                                                            "numeric", "numeric", "numeric", 
-                                                           "text", "blank", "blank", "date", 
-                                                           "date", "blank", "blank", "text", 
-                                                           "text", "text", "blank", "numeric", 
+                                                           "text", "skip", "skip", "date", 
+                                                           "date", "skip", "skip", "text", 
+                                                           "text", "text", "skip", "numeric", 
                                                            "numeric", "numeric", "numeric", 
-                                                           "numeric", "blank", "blank", "blank", 
-                                                           "blank", "blank", "text", "numeric", 
+                                                           "numeric", "skip", "skip", "skip", 
+                                                           "skip", "skip", "text", "numeric", 
                                                            "numeric", "text", "text", "text", 
-                                                           "blank"))
+                                                           "skip"))
 #fullcoord as a tibble
 fullcoord<-tbl_df(fullcoord)
 
 #Create the column day containing the day of the week of the home delivery
-fullcoord$day <- weekdays(as.Date(fullcoord$FechaDespacho)) 
+fullcoord$day <- weekdays(as.Date(fullcoord$FechaDespacho))
+fullcoord$Hcreacion<-format(fullcoord$Hcreacion,"%H:%M:%S")
+fullcoord$FechaDespacho<-format(fullcoord$FechaDespacho,"%d-%m-%Y")
+
+
+a<-fullcoord %>% 
+  group_by(ano,mes) %>%
+  summarise(demand = n()) %>%
+  ungroup() %>% 
+  mutate(Id = row_number())
+
 
 #################################################UI Shiny#########################################################
 header <- dashboardHeader(
@@ -53,78 +64,188 @@ header <- dashboardHeader(
 )
 
 body <- dashboardBody(
-  fluidRow(
-    column(width = 9,box(width = NULL, solidHeader = TRUE,leafletOutput("SFmap", height = 500),collapsible = T)),
-    #Tablero Lateral Superior#
-    column(width = 3,box(width = NULL, status = "primary",collapsible = T,
+  tabItems(
+    tabItem(tabName = "Mapa",
+        fluidRow(
+            column(width = 9,box(width = NULL, solidHeader = TRUE,leafletOutput("SFmap", height = 500),collapsible = T),
+                   box(width = NULL, plotOutput("Ventas"))),
+            
+            #Tablero Lateral Superior#
+            
+            column(width = 3,box(width = NULL, status = "primary",collapsible = T,
                          sliderInput("Year", "ano:",
-                                     min = as.numeric(min(fullcoord$ano, na.rm=TRUE)), max = as.numeric(max(fullcoord$ano, na.rm = TRUE)+1), value = c(as.numeric(min(fullcoord$ano, na.rm=TRUE)),as.numeric(max(fullcoord$ano, na.rm=TRUE)))),
+                                     min = as.numeric(min(fullcoord$ano, na.rm=TRUE)), max = as.numeric(max(fullcoord$ano, na.rm = TRUE)+1),
+                                     value = c(as.numeric(min(fullcoord$ano, na.rm=TRUE)),as.numeric(max(fullcoord$ano, na.rm=TRUE)))),
+                         
                          sliderInput("mes", "mes:",
                                      min = 1, max = 12, value = c(1,12)),
+                         
                          sliderInput("dia", "dia:",
                                      min = 1, max = 31, value = c(1,31))
-                        # selectInput(inputId = "Mes",
-                        #           label = "Seleccione el Mes",
-                        #             choices = c(1,2,3,4,5,6,7,8,9,10,11,12),
-                        #             selected = c(1,2,3,4,5,6,7,8,9,10,11,12)
-                        # )
+                        
+                            ),
+                   
+                          box(width = NULL ,status = "warning", collapsible = T,
+                            checkboxGroupInput("Super", "Ver Puntos de Retiro",choices = list("Mostrar Locales" = 1),
+                                                selected = 1)
+                              ),
+                          box(width = NULL ,status = "warning", collapsible = T,actionButton("action", label = "Update"))
+                  )
+                )
     ),
-    box(width = NULL ,status = "warning", collapsible = T,
-        checkboxGroupInput("Super", "Ver Puntos de Retiro",choices = list("Mostrar Locales" = 1),
-                           selected = 1)
+    
+    tabItem(tabName = "Datos",
+            fluidRow(
+              column(width = 10,
+              DT::dataTableOutput("table")
+            )
+            )
+    ),
+    
+    tabItem(tabName = "Archivos",
+            fluidRow(
+                     # App title ----
+                     titlePanel("Uploading Files"),
+                     
+                     # Sidebar layout with input and output definitions ----
+                     sidebarLayout(
+                       
+                       # Sidebar panel for inputs ----
+                       sidebarPanel(
+                         
+                         # Input: Select a file ----
+                         fileInput("file1", "Choose CSV File",
+                                   multiple = TRUE,
+                                   accept = c("text/csv",
+                                              "text/comma-separated-values,text/plain",
+                                              ".csv")),
+                         
+                         # Horizontal line ----
+                         tags$hr(),
+                         
+                         # Input: Checkbox if file has header ----
+                         checkboxInput("header", "Header", TRUE),
+                         
+                         # Input: Select separator ----
+                         radioButtons("sep", "Separator",
+                                      choices = c(Comma = ",",
+                                                  Semicolon = ";",
+                                                  Tab = "\t"),
+                                      selected = ","),
+                         
+                         # Input: Select quotes ----
+                         radioButtons("quote", "Quote",
+                                      choices = c(None = "",
+                                                  "Double Quote" = '"',
+                                                  "Single Quote" = "'"),
+                                      selected = '"'),
+                         
+                         # Horizontal line ----
+                         tags$hr(),
+                         
+                         # Input: Select number of rows to display ----
+                         radioButtons("disp", "Display",
+                                      choices = c(Head = "head",
+                                                  All = "all"),
+                                      selected = "head")
+                         
+                       ),
+                       
+                       # Main panel for displaying outputs ----
+                       mainPanel(
+                         
+                         # Output: Data file ----
+                         tableOutput("contents")
+                         
+                       )
+                       
+                     )
+                     
+              )
+            )
     )
+  )           
+
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard",tabName = "Mapa",icon = icon("dashboard")),
+    menuItem("Datos",tabName = "Datos",icon = icon("database")),
+    menuItem("Archivos",tabName = "Archivos",icon = icon("upload"))
     )
   )
-  
-)
-
 
 shinyApp(
   ui = dashboardPage( header,
-                      dashboardSidebar(disable = TRUE),
+                      sidebar,
                       body,skin = "green")  
   #################################################Shiny Server#########################################################
   
   ,
-  server = function(input, output) {
-    
-    
-    # Show only selected years
-    
-    
-    output$SFmap <- renderLeaflet({
-      UbicPedidos <- fullcoord
+  server = function(input, output, session) {
+    #Load CSV
+    output$contents <- renderTable({
       
-      if (length(UbicPedidos) == 0 || (max(fullcoord$ano)+1 == input$Year[1] & max(fullcoord$ano)+1 == input$Year[2]))
-        return(NULL)
+      # input$file1 will be NULL initially. After the user selects
+      # and uploads a file, head of that data file by default,
+      # or all rows if selected, will be shown.
       
+      req(input$file1)
       
-      # Show only selected years
-      UbicPedidos <- filter(UbicPedidos, ano  >= input$Year[1],ano <= input$Year[2], mes <= input$mes[2], mes >= input$mes[1], dia <= input$dia[2], dia >= input$dia[1])
-      Puntos <- filter(Puntos, Visible %in% as.numeric(input$Super))
+      df <- read.csv(input$file1$datapath,
+                     header = input$header,
+                     sep = input$sep,
+                     quote = input$quote)
       
+      if(input$disp == "head") {
+        return(head(df))
+      }
+      else {
+        return(df)
+      }
       
-      leaflet(fullcoord) %>%
-        #basic map
-        addTiles() %>%
-        setView(-70.6464,-33.4366, zoom = 10) %>% 
-        addLegend("bottomright", colors = "#03F", labels = "Comunas") %>%
-        
-        #Total
-        addMarkers(lng= ~(UbicPedidos$Longitude),
-                   lat = ~(UbicPedidos$Latitude),
-                   clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
-                   popup = ~(UbicPedidos$Direccion),
-                   label = ~(UbicPedidos$Direccion),
-                   labelOptions = labelOptions(noHide = F,
-                                               direction = 'auto')) %>% 
-        #Overlay Groups (multiple at time)
-        addAwesomeMarkers(lng=~(Puntos$Longitude), lat=~(Puntos$Latitude) ,icon=PP , popup = ~(Puntos$Direccion)) %>%
-        addPolygons(data=Comunas, color = "#3f3f3f", weight = 1, smoothFactor = 0.7,
-                    opacity = 1.0, fillOpacity = 0.5,
-                    highlightOptions = highlightOptions(color = "Blue", weight = 2,
-                                                        bringToFront = TRUE))
     })
-  }
-  
+    #Sales Graph
+    output$Ventas <- renderPlot({
+      plot<-ggplot(a,aes(Id,demand,group = 1)) + geom_line() + labs(title="Pedidos Online",x ="Periodo", y = "Cantidad Pedidos")
+      print(plot)
+    })
+    
+    #Data Table
+    output$table <- DT::renderDataTable(DT::datatable(fullcoord, options = list(scrollX = TRUE)))
+      
+      output$SFmap <- renderLeaflet({
+        UbicPedidos <- fullcoord
+        
+        if (length(UbicPedidos) == 0 || (max(fullcoord$ano)+1 == input$Year[1] & max(fullcoord$ano)+1 == input$Year[2]))
+          return(NULL)
+        
+        
+        # Show only selected years
+        UbicPedidos <- filter(UbicPedidos, ano  >= input$Year[1],ano <= input$Year[2], mes <= input$mes[2], mes >= input$mes[1], dia <= input$dia[2], dia >= input$dia[1])
+        Puntos <- filter(Puntos, Visible %in% as.numeric(input$Super))
+        
+        
+        leaflet(fullcoord) %>%
+          #basic map
+          addTiles() %>%
+          setView(-70.6464,-33.4366, zoom = 10) %>% 
+          addLegend("bottomright", colors = "#03F", labels = "Comunas") %>%
+          
+          #Total
+          addMarkers(lng= ~(UbicPedidos$Longitude),
+                     lat = ~(UbicPedidos$Latitude),
+                     clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
+                     popup = ~(UbicPedidos$Direccion),
+                     label = ~(UbicPedidos$Direccion),
+                     labelOptions = labelOptions(noHide = F,
+                                                 direction = 'auto')) %>% 
+          #Overlay Groups (multiple at time)
+          addAwesomeMarkers(lng=~(Puntos$Longitude), lat=~(Puntos$Latitude) ,icon=PP , popup = ~(Puntos$Direccion)) %>%
+          addPolygons(data=Comunas, color = "#3f3f3f", weight = 1, smoothFactor = 0.7,
+                      opacity = 1.0, fillOpacity = 0.5,
+                      highlightOptions = highlightOptions(color = "Blue", weight = 2,
+                                                          bringToFront = TRUE))
+      })
+    }
 )
